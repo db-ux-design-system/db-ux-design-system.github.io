b523d990d068945a37384855d6bfbfa0845dd425
TobiasOberpaul <Tobias.Oberpaul@deutschebahn.com>
2025-11-28 12:01:28 +0100

    Added Avatar Component & Avatar Showcase

--- template/components/index.ts
+++ template/components/index.ts
@@ -3,3 +3,4 @@ export { default as CTA } from './CTA/CTA.astro';
 export { default as PageHero } from './PageHero/PageHero.astro';
 export { default as SectionTitle } from './SectionTitle/SectionTitle.astro';
 export { default as Carousel } from './Carousel/Carousel.astro';
+export { default as Avatar } from './Avatar/Avatar.astro';
