3e174b3954ea44a51554fe49bcb53a6c21bcfe3b
TobiasOberpaul <Tobias.Oberpaul@deutschebahn.com>
2025-12-01 13:21:56 +0100

    Added "DisabledWrapper" Component

--- template/components/index.ts
+++ template/components/index.ts
@@ -4,3 +4,4 @@ export { default as PageHero } from './PageHero/PageHero.astro';
 export { default as SectionTitle } from './SectionTitle/SectionTitle.astro';
 export { default as Carousel } from './Carousel/Carousel.astro';
 export { default as Avatar } from './Avatar/Avatar.astro';
+export { default as DisabledWrapper } from './DisabledWrapper/DisabledWrapper.astro';
