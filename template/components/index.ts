b097e7b48db882ad764f9c639bc1449157c7d88c
TobiasOberpaul <Tobias.Oberpaul@deutschebahn.com>
2025-12-01 20:24:29 +0100

    Added Interactive Demo

--- template/components/index.ts
+++ template/components/index.ts
@@ -5,3 +5,4 @@ export { default as SectionTitle } from './SectionTitle/SectionTitle.astro';
 export { default as Carousel } from './Carousel/Carousel.astro';
 export { default as Avatar } from './Avatar/Avatar.astro';
 export { default as DisabledWrapper } from './DisabledWrapper/DisabledWrapper.astro';
+export { default as InteractiveDemo } from './InteractiveDemo/InteractiveDemo.astro';
