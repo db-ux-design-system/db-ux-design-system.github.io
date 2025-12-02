7fd82f70472f9a86f20fb9f3fe485dc68bd87fa0
michaelmkraus <michael.m.kraus@deutschebahn.com>
2025-12-02 11:09:39 +0100

    refactor(): move home-content into separate folder and extract configuration data

---
+++ content/pages/home/home.config.ts
@@ -0,0 +1,10 @@
+import { appConfig } from '@config';
+
+export const interactiveDemoOptions = [
+    { id: 'neutral-light', color: 'neutral', variant: 'filled', src: `${appConfig.basePath}assets/interactive-demo/Neutral--Lightmode--C.png`, label: 'Neutral' },
+    { id: 'db-light', icon: 'db', color: 'red', variant: 'filled', src: `${appConfig.basePath}assets/interactive-demo/DB--Lightmode--C.png`, label: 'Deutsche Bahn' },
+    { id: 'sbahn-light', icon: 's_bahn', color: 'green', variant: 'filled', src: `${appConfig.basePath}assets/interactive-demo/SB--Lightmode--C.png`, label: 'S-Bahn' },
+    { id: 'ri-light', icon: 'train_station', color: 'blue', variant: 'filled', src: `${appConfig.basePath}assets/interactive-demo/RI--Lightmode--C.png`, label: 'Station & Service' },
+]
+
+export const interactiveDemoDefaultOptionId = 'db-light';
