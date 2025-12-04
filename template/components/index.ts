611c4359453103271f44a8cc0143109cf7572194
michaelmkraus <michael.m.kraus@deutschebahn.com>
2025-12-04 08:36:38 +0100

    feat(): correct import for ContactUsSection in all pages

--- template/components/index.ts
+++ template/components/index.ts
@@ -7,3 +7,4 @@ export { default as Avatar } from './Avatar/Avatar.astro';
 export { default as ImageToggle } from './ImageToggle/ImageToggle.astro';
 export { default as DisabledWrapper } from './DisabledWrapper/DisabledWrapper.astro';
 export { default as InteractiveDemo } from './InteractiveDemo/InteractiveDemo.astro';
+export { default as ContactUsSection } from './ContactUsSection/ContactUsSection.astro';
