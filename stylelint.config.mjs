baf03e68e8dad1a1ecec92134d180afc5dd63fe8
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2026-01-29 14:52:23 +0100

    fix: linting issues (#22)

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,7 +3,12 @@ export default {
 	extends: ['stylelint-config-standard'],
 	plugins: ['@db-ux/core-stylelint'],
 	rules: {
-		'db-ux/use-spacings': [true], // margins, paddings, gaps
+		'db-ux/use-spacings': [
+			true,
+			{
+				allowCalc: true,
+			},
+		], // margins, paddings, gaps
 		'db-ux/use-border-width': [true], // border-width & border
 		'db-ux/use-border-radius': [true], // border-radius
 		'db-ux/use-border-color': [true], // border-color & border
